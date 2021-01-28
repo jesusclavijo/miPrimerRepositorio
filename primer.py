0c4cab5fb9cbb29ae3b6d69e39944717dcb67eac
jesusclavijo <78152176+jesusclavijo@users.noreply.github.com>
2021-01-28 14:08:46 +0000

    Update primer.py

--- primer.py
+++ primer.py
@@ -1,2 +1,8 @@
 #primer fichero
 print("hola")
+letra = input("dame una letra :")
+
+print("lo que escribistes fue ", letra)
+
+numero = float(input" dame un numero ")
+print("lo que  escribistes fue ";numero)
